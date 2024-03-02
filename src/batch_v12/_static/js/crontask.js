0406f22fc52a970288df81153db0ed4ebcf6fa64
Luis Hernández <luis.hernandez@induxsoft.net>
2024-03-02 09:59:48 -0600

    agregar cruds para grupos de tareas y programar tareas

---
+++ src/batch_v12/_static/js/crontask.js
@@ -0,0 +1,19 @@
+var crontask =
+{
+    init()
+    {
+        const check_disabled = document.querySelector('#check_disabled');
+        const ipt_disabled = document.querySelector('#ipt_disabled');
+
+        if (check_disabled && ipt_disabled) check_disabled.addEventListener('change', e => { this.set_input_check_value(ipt_disabled, check_disabled) });
+    },
+
+    set_input_check_value(inputElement, checkElement)
+    {
+        inputElement.value = (checkElement.checked ? 1 : 0);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    crontask.init()
+});
