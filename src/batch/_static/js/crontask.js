53b61b3badc3763c7febe32c802488cc399e87a9
Luis Hernández <luis.hernandez@induxsoft.net>
2024-03-21 18:02:36 -0600

    update
    actualizar select para recibir programas

--- src/batch/_static/js/crontask.js
+++ src/batch/_static/js/crontask.js
@@ -1,11 +1,35 @@
 var crontask =
 {
+    crontask: null, url: "",
+
     init()
     {
+        const sel_jobsgroup = document.getElementById("sel_jobsgroup");
+        const sel_program = document.getElementById("sel_program");
+
+        sel_jobsgroup.addEventListener("change", () => {
+            this.change_program(sel_jobsgroup,sel_program);
+        })
+
         const check_disabled = document.querySelector('#check_disabled');
         const ipt_disabled = document.querySelector('#ipt_disabled');
 
         if (check_disabled && ipt_disabled) check_disabled.addEventListener('change', () => { this.set_input_check_value(ipt_disabled, check_disabled) });
+    },
+
+    change_program(jobgroup,sel_program)
+    {
+        InduxsoftCrudlModel.InvokeService("./?idGroup="+jobgroup.value, null,
+            success => {
+                let options = success.map(program => `<option value=${program}>${program}</option>`).join('/n');
+
+                sel_program.innerHTML = options;
+            },
+            failure => {
+                alert('Failure \n' + JSON.stringify(failure))
+            },
+            'GET', false
+        );
     },
 
     set_input_check_value(inputElement, checkElement)
